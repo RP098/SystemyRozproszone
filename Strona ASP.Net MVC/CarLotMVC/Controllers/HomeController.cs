ce485766a660bf2578ae3b1c465a02690932538d
Adam <44466509+RP098@users.noreply.github.com>
2022-05-23 20:30:12 +0200

    Dodanie strony MVC

---
+++ Strona ASP.Net MVC/CarLotMVC/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CarLotMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Firma Ag Super Car";
+
+            return View();
+        }
+        public ActionResult Multimedia()
+        {
+            ViewBag.Message = "Skontaktuj się już dziś";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Skontaktuj się już dziś";
+
+            return View();
+        }
+    }
+}
---
+++ Strona ASP.Net MVC/CarLotMVC/Controllers/InventoryController.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using System.Threading.Tasks;
+using AutoLotDAL;
+
+namespace CarLotMVC.Controllers
+{
+    public class InventoryController : Controller
+    {
+        private CarLot db = new CarLot();
+
+        // GET: Inventory
+        public ActionResult Index()
+        {
+            return View(db.Inventory.ToList());
+        }
+
+        // GET: Inventory/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Inventory/Create
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "Make,Color,CarNickName")] Inventory inventory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    Inventory inventory1 = new Inventory();
+                    await inventory1.AddAsync( inventory.Make, inventory.Color, inventory.CarNickName);
+                    db.SaveChanges();
+                    return RedirectToAction("Index");
+                }
+            }
+            catch(Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, $"Unable to create record: {ex.Message}");
+                return View(inventory);
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Edit/5
+        // Aby zapewnić ochronę przed atakami polegającymi na przesyłaniu dodatkowych danych, włącz określone właściwości, z którymi chcesz utworzyć powiązania.
+        // Aby uzyskać więcej szczegółów, zobacz https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task< ActionResult> Edit([Bind(Include = "CarId,Make,Color,CarNickName")] Inventory inventory)
+        {
+            try
+            {
+                if (ModelState.IsValid)
+                {
+                    db.Entry(inventory).State = EntityState.Modified;
+                    await db.SaveChangesAsync();
+                    return RedirectToAction("Index");
+                }
+                return View(inventory);
+            }catch(Exception ex)
+            {
+                ModelState.AddModelError(string.Empty, "Unable to save record" + ex.Message );
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventory/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = db.Inventory.Find(id);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventory/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int id)
+        {
+           return await Task.Run(()=>
+                {
+                Inventory inventory = db.Inventory.Find(id);
+                db.Inventory.Remove(inventory);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            });
+         }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
